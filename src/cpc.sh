81c594f988101f2ef677fb19a14297093fd49cc7
morelucks <luckykamshak@gmail.com>
2025-02-25 21:08:28 +0100

    updates

--- src/cpc.sh
+++ src/cpc.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/ sh
+
+BINARY_FILE="https://github.com/morelucks/bash_scripting/raw/refs/heads/main/target/release/cpc_rust"
+
+echo "downloading binary file"
+
+wget  "$BINARY_FILE"
+
+chmod +x cpc_rust
+
+sudo mv cpc_rust "/usr/local/bin/luckify"
+
+
+
